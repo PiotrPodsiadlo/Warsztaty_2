809962ff5c1fe4dc1bfc21a5266ed6364baaaeae
Piotr <piotrekchorzow@interia.eu>
2019-07-24 00:17:02 +0200

    bcrypt library added to project

--- src/example1/Main1.java
+++ src/example1/Main1.java
@@ -1,4 +1,8 @@
 package example1;
 
 public class Main1 {
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/example1/User.java
@@ -0,0 +1,64 @@
+package example1;
+
+import com.oracle.webservices.internal.api.databinding.DatabindingMode;
+import org.mindrot.jbcrypt.BCrypt;
+
+
+public class User {
+
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public User setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+
+    public void hashPassword(String password){
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+
+    }
+}
